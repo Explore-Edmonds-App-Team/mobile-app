e8031ee32add40f731e77705c59d61099c0e8e28
Paul McInnis <mcinnip@students.wwu.edu>
2017-05-26 14:07:03 -0700

    changed the about.js

--- about.js
+++ about.js
@@ -3,6 +3,7 @@ import Dimensions from 'Dimensions';
 import {
   Image,
   Navigator,
+  ScrollView,
   StyleSheet,
   Text,
   TouchableHighlight,
@@ -11,17 +12,26 @@ import {
 
 import Header from './commons/header';
 import {main} from './stylesheets/main-theme';
+import {styles} from './stylesheets/about-styles';
 
 export default class About extends Component {
   
   render() {
     return (
-            // TODO Actually create an About page
             <View style={main.container}>
               <View style={main.HeaderContainer}>
                 <Header title='About' navigator={this.props.navigator}/>
               </View>
-              <View style={main.bodyContainer}/>
+              <View style={main.bodyContainer}>
+                <ScrollView style={styles.scrollContainer}>
+                  <Text>
+                  Welcome to the Visit Edmonds app. {'\n\n'}
+                  Talk about the city. {'\n\n'}
+                  Talk about the sustainable cities project. {'\n\n'}
+                  Talk about the developers (us!)
+                </Text>
+                </ScrollView>
+              </View>
             </View>
             )
   }
---
+++ stylesheets/about-styles.js
@@ -0,0 +1,14 @@
+import {
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+
+var {screenH, screenW} = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    margin: 15,
+  }
+});
+
+export {styles};
